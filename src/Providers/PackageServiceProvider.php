64fe6a4a414aa3706d2c673f18ea46794a0c7e3f
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2018-08-26 21:02:36 +0700

    add logout api

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -23,14 +23,20 @@ class PackageServiceProvider extends ServiceProvider
         // API ROUTES
         $routeConfig = [
             'namespace' => 'GilbertRonaldo\CoreSystem\Controllers',
-            'prefix' => 'auth',
+            'prefix' => 'api/auth',
             'domain' => null,
+            'middleware' => 'api',
         ];
 
         $this->getRouter()->group($routeConfig, function($router) {
             $router->post('login', [
                 'uses' => 'AuthController@login',
                 'as' => 'auth.login',
+            ]);
+
+            $router->post('logout', [
+                'uses' => 'AuthController@logout',
+                'as' => 'auth.logout',
             ]);
         });
     }
